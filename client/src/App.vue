b4cbdebc614bcf70dab281e06be35c523a18e8d0
Filip <wojtasik.f03@htlwienwest.at>
2021-10-20 19:49:47 +0200

    DataTable erstellt, Select implementiert

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,16 @@
   <v-app>
     <v-app-bar app color="blue-grey lighten-5">
       <div class="d-flex align-center">
-        <v-img class="mr-2 mt-10" src="../public/Camera.png" width="100" />
-
+        <v-img class="mr-2 mt-10 hidden-md-and-down" src="../public/Camera.png" width="100" />
         <h1>Movies</h1>
       </div>
-
       <v-spacer></v-spacer>
-
       <v-btn href="https://www.imdb.com/movies-in-theaters/" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
+
 
     <v-main>
       <router-view />
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,19 @@
-<template> 
-<div>
-  {{this.movies}}
-</div>
+<template>
+  <div class="ml-15 mr-15">
+    <v-alert color="blue-grey" dark class="ma-10">
+      Rent selected movies: {{ selectedMovie.toString() }}
+    </v-alert>
+    <v-data-table
+      v-model="selectedItem"
+      :headers="headers"
+      :items="movies"
+      :items-per-page="6"
+      :footer-props="{ itemsPerPageOptions: [6, 12, 18] }"
+      show-select
+      class="elevation-1 text-center"
+    >
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -11,9 +23,17 @@ export default {
   data() {
     return {
       movies: [],
+      selectedItem: [],
+      headers: [
+        { text: 'Title', align: 'start', value: 'title', class: 'blue-grey lighten-5' },
+        { text: 'Director', value: 'director', class: 'blue-grey lighten-5' },
+        { text: 'Genre', value: 'main_genre', class: 'blue-grey lighten-5' },
+        { text: 'Year', value: 'year', width: 100, class: 'blue-grey lighten-5' },
+        { text: 'Rated', value: 'rated', width: 100, class: 'blue-grey lighten-5' },
+        { text: 'Plot ', value: 'plot' },
+      ],
     };
   },
-  components: {},
 
   methods: {
     async getMovies() {
@@ -24,6 +44,13 @@ export default {
       this.movies = data;
     },
   },
+
+  computed: {
+    selectedMovie() {
+      return this.selectedItem.map((el) => el.title);
+    },
+  },
+
   created() {
     this.getMovies();
   },
